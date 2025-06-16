1bfd782bd3525b2d7e81a2b32aab0959119654cc
egoo00 <egooo914@gmail.com>
2025-06-16 16:18:13 +0300

    Update DistanceController.java

--- src/main/java/com/example/distanceservice/controller/DistanceController.java
+++ src/main/java/com/example/distanceservice/controller/DistanceController.java
@@ -5,13 +5,12 @@ import com.example.distanceservice.service.DistanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api")
 public class DistanceController {
     private final DistanceService distanceService;
 
+    
     @Autowired
     public DistanceController(DistanceService distanceService) {
         this.distanceService = distanceService;
@@ -20,10 +19,5 @@ public class DistanceController {
     @GetMapping("/distance")
     public DistanceResponse getDistance(@RequestParam String from, @RequestParam String to) {
         return distanceService.calculateDistance(from, to);
-    }
-
-    @PostMapping("/distances/bulk")
-    public List<DistanceResponse> getBulkDistances(@RequestBody List<String[]> cityPairs) {
-        return distanceService.calculateBulkDistances(cityPairs);
     }
 }
